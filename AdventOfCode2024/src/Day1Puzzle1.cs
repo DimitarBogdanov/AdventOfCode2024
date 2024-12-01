7d9946c68ec60eb273edaa6f2308fc11720c1269
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2024-12-01 23:50:18 +0200

    Initial commit

---
+++ AdventOfCode2024/src/Day1Puzzle1.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024;
+
+public sealed class Day1Puzzle1 : IAdventProblem
+{
+    public int DayNumber => 1;
+    
+    public void Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventOfCode2024/src/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2024;
+
+public static class Program
+{
+    public static void Main()
+    {
+        IAdventProblem problem = new Day1Puzzle1();
+        string? input = GetInput(problem);
+        if (input == null)
+        {
+            Console.WriteLine("Input file not found, please add it to the executable directory with the name 'input_X.txt', where X = the day number");
+            return;
+        }
+        
+        problem.Solve(input);
+    }
+
+    private static string? GetInput(IAdventProblem forProblem)
+    {
+        int dayNumber = forProblem.DayNumber;
+        string targetPath = Path.Combine(Environment.CurrentDirectory, $"input_{dayNumber}.txt");
+        return File.Exists(targetPath)
+            ? File.ReadAllText(targetPath)
+            : null;
+    }
+}
