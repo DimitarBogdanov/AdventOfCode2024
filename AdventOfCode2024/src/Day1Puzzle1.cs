105f644577e6a8215792ed435617f00ecad64fdc
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2024-12-02 00:06:33 +0200

    Solve Day 1 Part 1

--- AdventOfCode2024/src/Day1Puzzle1.cs
+++ AdventOfCode2024/src/Day1Puzzle1.cs
@@ -6,6 +6,40 @@ public sealed class Day1Puzzle1 : IAdventProblem
     
     public void Solve(string input)
     {
-        throw new NotImplementedException();
+        string[] lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        int[] leftList = new int[lines.Length];
+        int[] rightList = new int[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] nums = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            leftList[i] = Int32.Parse(nums[0]);
+            rightList[i] = Int32.Parse(nums[1]);
+        }
+
+        long totalDist = 0;
+        
+        for (int i = 0; i < leftList.Length; i++)
+        {
+            int idxOfMinLeft = IndexOfMin(leftList);
+            int idxOfMinRight = IndexOfMin(rightList);
+            
+            int numLeft = leftList[idxOfMinLeft];
+            int numRight = rightList[idxOfMinRight];
+            
+            int distance = Math.Abs(numLeft - numRight);
+            totalDist += distance;
+            
+            leftList[idxOfMinLeft] = Int32.MaxValue;
+            rightList[idxOfMinRight] = Int32.MaxValue;
+        }
+
+        Console.WriteLine(totalDist);
+    }
+
+    private static int IndexOfMin(int[] array)
+    {
+        int min = array.Min();
+        int idx = Array.IndexOf(array, min);
+        return idx;
     }
 }
