2611871d6380e66ecbbb7d7a2c555f5a408df807
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2024-12-04 11:42:29 +0200

    Solve Day 4 Part 2

---
+++ AdventOfCode2024/src/Day4Part2.cs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode2024;
+
+public sealed class Day4Part2 : IAdventProblem
+{
+    public int DayNumber => 4;
+    
+    public void Solve(string input)
+    {
+        char[][] masks =
+        {
+            new[] {
+                'M', ' ', 'M',
+                ' ', 'A', ' ',
+                'S', ' ', 'S'
+            },
+            new[] {
+                'S', ' ', 'M',
+                ' ', 'A', ' ',
+                'S', ' ', 'M'
+            },
+            new[] {
+                'S', ' ', 'S',
+                ' ', 'A', ' ',
+                'M', ' ', 'M'
+            },
+            new[] {
+                'M', ' ', 'S',
+                ' ', 'A', ' ',
+                'M', ' ', 'S'
+            },
+        };
+        
+        string[] lines = input.Split('\n');
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i] = lines[i].Trim();
+        }
+
+        int h = lines.Length;
+        int w = lines[0].Length;
+        char[,] matrix = new char[w, h];
+        int lineCounter = 0;
+        foreach (string line in lines)
+        {
+            for (int i = 0; i < w; i++)
+            {
+                matrix[i, lineCounter] = line[i];
+            }
+
+            lineCounter++;
+        }
+
+        int count = 0;
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                // Normal
+                if (IsWithinBounds(h, w, x + 2, y + 2))
+                {
+                    foreach (char[] mask in masks)
+                    {
+                        if (MaskApplies(matrix, x, y, mask))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    private static bool MaskApplies(char[,] matrix, int x, int y, char[] mask)
+    {
+        return matrix[x, y] == mask[0]
+               && matrix[x + 2, y] == mask[2]
+               && matrix[x + 1, y + 1] == mask[4]
+               && matrix[x, y + 2] == mask[6]
+               && matrix[x + 2, y + 2] == mask[8];
+    }
+    
+    private static bool IsWithinBounds(int height, int width, int x, int y)
+    {
+        return x >= 0 && x < width
+                      && y >= 0 && y < height;
+    }
+}
